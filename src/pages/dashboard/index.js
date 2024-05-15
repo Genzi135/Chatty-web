48a4fd4c31f7e1c50a5872b28b775ef52ded6d1b
Genzi135 <phu13052002@gmail.com>
2024-05-15 20:59:19 +0700

    debugg rerender useEffect

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -4,31 +4,39 @@ import SideBar from "./sideBar/SideBar";
 import SubSideBar from "./sideBar/SubSideBar";
 import Contact from "./contact";
 import { useSocket } from "../../hooks/context/socket";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { addMessage, setCurrentConversation, setListConversation, setListMessage } from "../../hooks/redux/reducer";
-import ConversationSkeleton from "../../components/common/ConversationSkeleton";
 import { ToastContainer, toast } from "react-toastify";
-import 'react-toastify/ReactToastify.css'
+import 'react-toastify/ReactToastify.css';
 
 export default function Dashboard() {
-    const viewState = useSelector(state => state.view)
-    const { socket } = useSocket()
-    const currentConversation = useSelector(state => state.currentConversation)
-    const listConversation = useSelector(state => state.listConversation)
-    const listMessage = useSelector(state => state.listMessage)
+    const viewState = useSelector(state => state.view);
+    const { socket } = useSocket();
+    const currentConversation = useSelector(state => state.currentConversation);
+    const listConversation = useSelector(state => state.listConversation);
+    const listMessage = useSelector(state => state.listMessage);
 
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
+
+    const currentConversationRef = useRef(currentConversation);
+    const listConversationRef = useRef(listConversation);
+    const listMessageRef = useRef(listMessage);
 
     useEffect(() => {
-        socket.on("message:receive", (response) => {
-            if (currentConversation._id === response.conversation._id) {
-                //dispatch(updateConversationLastMessage(response.conversation._id, response));
-                dispatch(addMessage(response))
+        currentConversationRef.current = currentConversation;
+        listConversationRef.current = listConversation;
+        listMessageRef.current = listMessage;
+    }, [currentConversation, listConversation, listMessage]);
+
+    useEffect(() => {
+        const handleReceiveMessage = (response) => {
+            if (currentConversationRef.current._id === response.conversation._id) {
+                dispatch(addMessage(response));
             }
 
-            const newList = listConversation.map(conversation => {
+            const newList = listConversationRef.current.map(conversation => {
                 if (conversation._id === response.conversation._id) {
-                    if (conversation._id === currentConversation._id) {
+                    if (conversation._id === currentConversationRef.current._id) {
                         return { ...conversation, lastMessage: response, isReadMessage: true };
                     }
                     return { ...conversation, lastMessage: response, isReadMessage: false };
@@ -36,57 +44,58 @@ export default function Dashboard() {
                 return conversation;
             });
 
-            dispatch(setListConversation(newList))
-        });
-    }, [currentConversation]);
+            dispatch(setListConversation(newList));
+        };
 
-    useEffect(() => {
-        socket.on("message:deleted", (response) => {
-            const newList = listMessage.map((e) => {
+        const handleDeleteMessage = (response) => {
+            const newList = listMessageRef.current.map((e) => {
                 if (response.id === e._id) {
-                    return { ...e, content: "This message has been deleted", isDelete: true }
+                    return { ...e, content: "This message has been deleted", isDelete: true };
                 }
                 return e;
-            })
-            dispatch(setListMessage(newList))
-        })
-    }, [listMessage])
+            });
+            dispatch(setListMessage(newList));
+        };
 
-    useEffect(() => {
-        socket.on("message:notification", (data) => {
-            if (currentConversation._id) {
+        const handleNotification = (data) => {
+            if (currentConversationRef.current._id) {
                 if (data) {
-                    //console.log(currentConversation._id.toString() === data.conversationId.toString())
-                    console.log(data)
-                    console.log(currentConversation)
-                    if (currentConversation._id === data.conservationId) {
-                        dispatch(addMessage(data.messages[0]))
-                        toast(data.messages[0].content)
-                        dispatch(setCurrentConversation(data.conversation))
+                    if (currentConversationRef.current._id === data.conservationId) {
+                        dispatch(addMessage(data.messages[0]));
+                        toast(data.messages[0].content);
+                        dispatch(setCurrentConversation(data.conversation));
                     }
                 }
             }
-        })
-    })
+        };
 
-    useEffect(() => {
-        socket.on("conversation:disband", (data) => {
-            if (data.conservationId === currentConversation._id) {
-                dispatch(setCurrentConversation({}))
+        const handleDisbandConversation = (data) => {
+            if (data.conservationId === currentConversationRef.current._id) {
+                dispatch(setCurrentConversation({}));
             }
-            const newList = listConversation.filter(e => e._id !== data.conservationId)
-            console.log(newList)
-            dispatch(setListConversation(newList))
-        })
-    })
+            const newList = listConversationRef.current.filter(e => e._id !== data.conservationId);
+            dispatch(setListConversation(newList));
+        };
 
-    useEffect(() => {
-        socket.on("conversation:new", (data) => {
-            const newList = [data.conversation, ...listConversation]
-            console.log(listConversation)
-            dispatch(setListConversation(newList))
-        })
-    })
+        const handleNewConversation = (data) => {
+            const newList = [data.conversation, ...listConversationRef.current];
+            dispatch(setListConversation(newList));
+        };
+
+        socket.on("message:receive", handleReceiveMessage);
+        socket.on("message:deleted", handleDeleteMessage);
+        socket.on("message:notification", handleNotification);
+        socket.on("conversation:disband", handleDisbandConversation);
+        socket.on("conversation:new", handleNewConversation);
+
+        return () => {
+            socket.off("message:receive", handleReceiveMessage);
+            socket.off("message:deleted", handleDeleteMessage);
+            socket.off("message:notification", handleNotification);
+            socket.off("conversation:disband", handleDisbandConversation);
+            socket.off("conversation:new", handleNewConversation);
+        };
+    }, [socket, dispatch]);
 
     return (
         <div style={{ width: '100vw', height: '100vh', display: 'flex', alignItems: 'center', position: 'relative' }}>
@@ -96,5 +105,5 @@ export default function Dashboard() {
             {viewState.subSideBar === "contact" && <Contact />}
             <div className="absolute bottom-2 right-2"><ToastContainer /></div>
         </div>
-    )
+    );
 }
