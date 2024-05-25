13c864339c1d3b70aeaa3fb56031b4443dc07b9f
Huy Trần Lê <tranlehuy0407@gmail.com>
2024-05-25 10:42:11 +0700

    final debuging v2

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -146,7 +146,7 @@ export default function Dashboard() {
             if (data.conversation.members.some(e => e._id === currentUserRef.current._id)) {
                 const newList = [data.conversation, ...listConversationRef.current];
                 dispatch(setListConversation(newList));
-                toast("Create new group successful")
+                toast("New group created")
             } else {
                 console.log('nothing');
             }
